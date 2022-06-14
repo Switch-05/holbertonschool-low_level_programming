f83ddcfedbcba0fa57db83d32ad6dfe019a88211
Switch <david.gamez.m@gmail.com>
2022-06-13 22:08:43 -0500

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This program prints exactly a text followed by a new line.
+ *
+ * Description: You cant use puts, insted use printf.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
