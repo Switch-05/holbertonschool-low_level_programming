835d09c8f68250e32be7248c97f15e3ab10951b1
Switch <david.gamez.m@gmail.com>
2022-06-21 11:52:30 -0500

    functions

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ *File: 0-putchar.c
+ *author: David G
+ */
+
+#include "main.h"
+/**
+ *Main - program that prints _putchar.
+ *
+ *Description: program that prints _putchar.
+ *@void: void.
+ *return: always 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
