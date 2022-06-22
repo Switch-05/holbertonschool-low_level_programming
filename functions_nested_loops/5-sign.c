b07d59af3f4cedc423d3a31e3f5b034cc1e56eff
Switch <david.gamez.m@gmail.com>
2022-06-22 08:46:50 -0500

    numbersign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * file: 5-sign.c
+ * author: David G
+ */
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: Integer to be tested.
+ *
+ * Description: Prints the sign of a number.
+ * Return: 1 if positive, 0 if zero, -1 if negative.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
