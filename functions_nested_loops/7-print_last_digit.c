1b79c193b9fc607afd3d7ea94bcfc55d6f0aed45
Switch <david.gamez.m@gmail.com>
2022-06-27 15:03:20 -0500

    lastdigit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+/*
+ * file: 4-isalpha.c
+ * author: David G
+ */
+#include "main.h"
+
+/**
+ * print_last_digit - Checks for alphabetic character.
+ * @n: Character to be tested
+ *
+ * Description: Checks if lowercase or uppercase.
+ * Return: 1 if c is a letter, 0 otherwise.
+ */
+int print_last_digit(int n)	
+{
+	int num;
+
+	num = n % 10;
+	if (num < 0)
+	{
+		num = num * -1;
+	}
+	_putchar(num + 48);
+	return (num);
+}
